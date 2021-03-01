7c60eb093823294ca322566d1b7467f6f61d1f26
Emma <ecbaskerville@yahoo.com>
2021-03-01 15:57:36 +0000

    Add 'esc' exit and 'pyautogui.move'

--- GestureAnalysis.py
+++ GestureAnalysis.py
@@ -1,64 +1,64 @@
 
 import numpy as np
-import cv2 as cv
-import pyautogui, sys #Used to import support for mouse functions
+import cv2, sys, pyautogui # import mouse & keyboard control
 
-
-cap = cv.VideoCapture(0)
-
-#frame_width = int(cap.get(3))
-#frame_height = int(cap.get(4))
+# capture video input
+cap = cv2.VideoCapture(0)
 
 while(True):
-	#Stores the frame from vid
+
+	# store video frame, flip, resize, and set resolution
 	ret, frame = cap.read()
-	#flips the image 
-	frame = cv.flip(frame,1)
-	#Sets screen to full res
-	frame = cv.resize(frame,(1920,1080), fx=0,fy=0, interpolation= cv.INTER_CUBIC)
+	frame = cv2.flip(frame,1)
+	frame = cv2.resize(frame,(1920,1080), fx=0,fy=0, interpolation= cv2.INTER_CUBIC)
 	
-	
-	#Converts image from RGB to HSV
-	hsvim = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-	#Stores the lowest and highest HSV values
+	# convert frame from RGB to HSV
+	hsvim = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	# store lowest and highest HSV values
 	lower = np.array([21, 79, 0], dtype = np.uint8)
 	upper = np.array([70, 135, 242], dtype = np.uint8)
-	skinRegionHSV = cv.inRange(hsvim, lower, upper)
+	skinRegionHSV = cv2.inRange(hsvim, lower, upper)
 
-	#blurs the image and thresholding
-	blurred = cv.GaussianBlur(skinRegionHSV,(5,5),0)
-	ret,thresh = cv.threshold(blurred,240,255,cv.THRESH_BINARY)
+	# blur image & set threshold
+	blurred = cv2.GaussianBlur(skinRegionHSV,(5,5),0)
+	ret,thresh = cv2.threshold(blurred,240,255,cv2.THRESH_BINARY)
 	
-	#Stores the image if image is within the HSV range if no contour is present the if statement is skipped
-	contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+	# store image if it's within the HSV range - if no contour is present then skip if statement
+	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 	if(contours != []):
 
-		#Draws the contours around the selected HSV values
-		contours = max(contours, key=lambda x: cv.contourArea(x))
-		cv.drawContours(frame, [contours], -1, (255,255,0), 2)
+		# draw contours around the selected HSV values
+		contours = max(contours, key=lambda x: cv2.contourArea(x))
+		cv2.drawContours(frame, [contours], -1, (255,255,0), 2)
 
-		#Draws a hull around the contour previosuly created
-		hull = cv.convexHull(contours)
-		cv.drawContours(frame, [hull], -1, (0, 255, 255), 2)
-		#Stores the values for the centre of the Hull
-		M = cv.moments(hull)
+		# draw a hull around the contour previosuly created
+		hull = cv2.convexHull(contours)
+		cv2.drawContours(frame, [hull], -1, (0, 255, 255), 2)
+
+		# store the values for the centre of the hull
+		M = cv2.moments(hull)
 		cX = int(M["m10"] / M["m00"])
 		cY = int(M["m01"] / M["m00"])
 
-		#Draws the circle in the middle
-		cv.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+		# draw circle in centre of the hull
+		cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
 
-		#Used to control mouse functions
+		# move cursor to points
 		pyautogui.moveTo(cX, cY) 
 
-	#Shows the frame
-	cv.imshow("track", frame)
+		# pyautogui.move(oldX - newX, oldY - newY, duration=1)  # move mouse relative to its current position
 
-	#Used to end loop
-	ch = cv.waitKey(1)
-	if ch & 0xFF == ord('q'):
+	# show the frame
+	cv2.imshow("Frame", frame)
+
+	# end loop on 'esc'
+	ch = cv2.waitKey(1)
+	if ch & 0xFF == 27:
 		break
 
+# release resources and destroy open windows
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
 
